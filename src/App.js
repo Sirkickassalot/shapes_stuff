4d42829663fa7faa792e9f297015cf528878dc93
Isaac De La Cruz <89318065+Sirkickassalot@users.noreply.github.com>
2022-01-09 15:21:52 -0800

    added styling

--- src/App.js
+++ src/App.js
@@ -3,8 +3,12 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <div className="circle"></div>
-      
+      <div className="circle_1"></div>
+      <div className="circle_2"></div>
+      <div className="circle_3"></div>
+        <div className="searchBar">
+          <input className='searchInput' placeholder='Type Color'/>
+        </div>  
     </div>
   );
 }
